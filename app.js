063229960d48fa37e7c98dcead22e02cd497b004
alijafri691 <alijafri691@gmail.com>
2025-02-01 18:11:36 +0530

    Updated backend logic

--- app.js
+++ app.js
@@ -25,19 +25,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Database connection
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
